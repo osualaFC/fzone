aaac78b5d11baa3f6f6c8aa1e79551f12d5c415f
Fredrick Osuala <fredrick.osuala@gmail.com>
2022-01-29 09:34:37 +0100

    release build

--- app/src/androidTest/java/com/fredosuala/fzone/SignUpRobot.kt
+++ app/src/androidTest/java/com/fredosuala/fzone/SignUpRobot.kt
@@ -1,9 +1,27 @@
 package com.fredosuala.fzone
 
-fun launchSignUpScreen(
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 
+fun launchSignUpScreen(
+    rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
+    block: SignUpRobot.() -> Unit
 ) : SignUpRobot {
-    return SignUpRobot()
+    return SignUpRobot().apply(block)
 }
 
-class SignUpRobot
+class SignUpRobot {
+    fun typeEmail(email : String) {}
+    fun typePassword(password: String) {}
+    fun submit(){}
+
+    infix fun verify(
+        block: SignUpVerification.() -> Unit
+    ): SignUpVerification {
+        return  SignUpVerification().apply(block)
+    }
+}
+
+class SignUpVerification {
+    fun timelineScreenIsPresent() {}
+}
--- app/src/androidTest/java/com/fredosuala/fzone/SignUpTest.kt
+++ app/src/androidTest/java/com/fredosuala/fzone/SignUpTest.kt
@@ -1,11 +1,21 @@
 package com.fredosuala.fzone
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
 import org.junit.Test
 
 class SignUpTest {
 
+    @get:Rule
+    val signUpTestRule = createAndroidComposeRule<MainActivity>()
     @Test
     fun performSignUp() {
-
+        launchSignUpScreen {
+            typeEmail("test@test@gmail.com")
+            typePassword("password")
+            submit()
+        } verify {
+            timelineScreenIsPresent()
+        }
     }
 }
